51d214f9051c3d2cf4a29a236ee66c31c94cace8
mr.scofield <72885918+nightcoffee4@users.noreply.github.com>
2021-04-08 13:54:43 +0300

    ilk commit
    08/04/2021 13:54

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+
+function HomePage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Anasayfa</title>
+      </Head>
+      <h1>Welcome to Antalya</h1>
+
+      <style jsx>{``}</style>
+    </Layout>
+  );
+}
+
+export default HomePage;
